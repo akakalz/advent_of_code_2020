17c62d10c2317b023f5a4fdfe3efd492ad2e9e80
Andrew Kalz <andrewkalz@gmail.com>
2020-12-03 08:56:40 -0600

    AK - Adding more parameterized tests cases for the full part1/2 test of day 3

--- tests/test_days.py
+++ tests/test_days.py
@@ -37,12 +37,29 @@ def test_day_2():
         ((3, 3), 12),
         ((4, 4), 9),
         ((5, 5), 7),
+        ((1, 3), 2),
+        ((1, 5), 2),
+        ((1, 7), 2),
+        ((2, 1), 1),
     ]
 )
-def test_day_3(slope, expected_trees):
+def test_day_3_traversal(slope, expected_trees):
     # arrange
     test_obj = Day3('input/day_three_input.txt')
     # act
     actual_trees = test_obj._traverse_slope(slope)
     # assert
     assert actual_trees == expected_trees
+
+
+def test_day_3():
+    # arrange
+    test_obj = Day3('input/day_three_input.txt')
+    expected_part_1_answer = 2
+    expected_part_2_answer = 38 * 2 * 2 * 2 * 1
+    # act
+    actual_part_1_answer = test_obj.part_1()
+    actual_part_2_answer = test_obj.part_2()
+    # assert
+    assert actual_part_1_answer == expected_part_1_answer
+    assert actual_part_2_answer == expected_part_2_answer
