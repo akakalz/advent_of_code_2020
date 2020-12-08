ab5cbef69289d9c678d948747caf714c594944cf
Andrew Kalz <andrewkalz@gmail.com>
2020-12-07 23:38:54 -0600

    AK - day 8 prep

---
+++ src/day_08/__init__.py
@@ -0,0 +1,4 @@
+from .day_08 import Day8
+
+
+__all__ = [Day8]
---
+++ src/day_08/day_08.py
@@ -0,0 +1,15 @@
+# https://adventofcode.com/2020/day/8
+
+from day import Day
+# import re
+
+
+class Day8(Day):
+    def __init__(self, file_name):
+        super().__init__(8, file_name)
+
+    def part_1(self):
+        return None
+
+    def part_2(self):
+        return None
